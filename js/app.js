6562dce09d398ee93d2fdfa80f25b9cdfc073da8
lili hualtibamba <lilihperezz@gmail.com>
2016-08-25 15:14:05 -0500

    agregando files suma

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+
+var numero1= prompt("Ingrese primer número");
+var numero2 = prompt("Ingrese segundo número");
+
+	var resultado = (parseInt(numero1)+parseInt(numero2));
+	document.write('<br>');
+	document.write("El resultado de la suma es " + resultado);
